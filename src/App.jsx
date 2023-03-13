6947d25f1dedfe68b393ac0cc476213305ef5625
vignesh1073 <s.vigneshwaran.cse21@iitbhu.ac.in>
2023-03-14 00:36:35 +0530

    unresponsivenavbaradded

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter, Routes,  Route } from 'react-router-dom';
 import Users from './scenes/Users';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+    <Navbar />
+    <br></br>
     <Routes>
       <Route path="/" element={<Users />}></Route>
     </Routes>
